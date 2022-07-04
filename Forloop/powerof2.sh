b0b263976f437af06e517704b10e9c874040b319
Anusha <anushavysakh232@gmail.com>
2022-07-04 22:37:21 +0530

    Practise on For loop

---
+++ Forloop/powerof2.sh
@@ -0,0 +1,13 @@
+#1. Write a program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n.
+
+echo "Enter a base number:"
+read base
+echo  "Enter a exponential number:"
+read exp
+power=1
+for (( i=$exp; i!=0; i-- ))
+do
+    power=$(($power*$base))
+
+done
+echo $power
